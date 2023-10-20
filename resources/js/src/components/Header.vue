460d93bd1475694ce6315fdb0a9288d1f978f863
Maksim Aksenov <ggyoug@gmail.com>
2023-10-20 12:19:49 +0300

    feat: add fonts

--- resources/js/src/components/Header.vue
+++ resources/js/src/components/Header.vue
@@ -3,15 +3,21 @@
         <div class="header">
             <div class="header-left">
                 <div class="header-left__logo">
-                    logo
+                    Crisp
                 </div>
                 <div class="header-left__nav">
-                    nav
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Sale</a></li>
+                        <li><a href="#">Contact us</a></li>
+                    </ul>
                 </div>
             </div>
             <div class="header-right">
                 <div class="header-right__search">
-                    search
+                    Search
                 </div>
                 <div class="header-right-block">
                     <div class="header-right-block__auth">
@@ -27,30 +33,6 @@
             </div>
         </div>
     </div>
-<!--    <nav class="navbar navbar-expand-lg bg-body-tertiary">-->
-<!--        <div class="container">-->
-<!--            <div class="navbar-brand">Super History</div>-->
-<!--            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"-->
-<!--                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"-->
-<!--                    aria-expanded="false" aria-label="Toggle navigation">-->
-<!--                <span class="navbar-toggler-icon"></span>-->
-<!--            </button>-->
-<!--            <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
-<!--                <ul class="navbar-nav me-auto mb-2 mb-lg-0">-->
-
-<!--                    <li class="nav-item" v-for="(item, index) in headerMenuList" :key="index">-->
-<!--                        <router-link :to="{name: item.routeName}" class="nav-link" aria-current="page">-->
-<!--                            {{ item.value }}-->
-<!--                        </router-link>-->
-<!--                    </li>-->
-
-<!--                </ul>-->
-<!--                <div>-->
-<!--                    Register|Login-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </nav>-->
 </template>
 
 <script>
@@ -82,8 +64,44 @@ export default {
         display: flex;
         align-items: center;
         justify-content: space-between;
-        background-color: gold;
         width: 50%;
+        &__logo {
+            color: #fff;
+            font-size: 15px;
+            text-transform: uppercase;
+            margin-left: 27px;
+            padding: 5px 9px;
+            border: 2px solid #fff;
+            height: 38px;
+        }
+        &__nav {
+            ul {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                height: 60px;
+                li {
+                    padding: 0 15px;
+                    text-transform: uppercase;
+                    a {
+                        position: relative;
+                        color: #fff;
+                        text-decoration: none;
+                        &:after {
+                            content: '';
+                            position: absolute;
+                            left: 0; bottom: -9px;
+                            width: 100%;
+                            height: 2px;
+                            background-color: #EB5757;
+                        }
+                        &:hover {
+
+                        }
+                    }
+                }
+            }
+        }
     }
     &-right {
         display: flex;
@@ -91,6 +109,9 @@ export default {
         justify-content: space-between;
         background-color: green;
         width: 50%;
+        &__search {
+            padding: 0;
+        }
         &-block {
             display: flex;
             align-items: center;
