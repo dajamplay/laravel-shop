db11527d6b9615ea0f8a000f89ede8dacd69712e
dajamplay <ggyoug@gmail.com>
2023-11-06 19:48:14 +0300

    feat: add columns table to show products

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,27 +1,33 @@
 @extends('admin')
-
+@php
+/** @var $product \App\Data\ProductData */
+@endphp
 @section('title', __('Продукция'))
 
 @section('content')
-
-    <x-ui.button
-        text="{{__('Создать продукт')}}"
-        href="{{ route('admin.products.create') }}"
-    />
 
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
 
+                <x-ui.button
+                    text="{{__('Создать продукт')}}"
+                    href="{{ route('admin.products.create') }}"
+                />
+
                 @if($products->isNotEmpty())
 
-                    <table class="table table-hover mt-3">
+                    <table class="table table-striped table-bordered table-hover projects mt-3">
                         <thead>
                             <tr>
                                 <th>ID</th>
                                 <th>Наименование</th>
-                                <th>Стоимость</th>
+                                <th>Цена</th>
+                                <th>Цена(опт)</th>
+                                <th>Объем</th>
                                 <th>Описание</th>
+                                <th>Линия</th>
+                                <th>Бренд</th>
                                 <th>Изображение</th>
                                 <th>Действия</th>
                             </tr>
@@ -30,11 +36,15 @@
                             @foreach($products as $product)
                                 <tr>
                                     <td>{{$product->id}}</td>
-                                    <td>{{$product->title}}</td>
-                                    <td>{{$product->price}}</td>
-                                    <td>{{$product->content}}</td>
+                                    <td><a href="{{ route('admin.products.show', $product) }}">{{$product->title}}</a></td>
+                                    <td>{{$product->price}} {{"\u{20BD}"}}</td>
+                                    <td>{{$product->price_opt}} {{"\u{20BD}"}}</td>
+                                    <td>{{$product->size}} мл</td>
+                                    <td>{!! Str::limit($product->content, 20, ' ...') !!}</td>
+                                    <td>{{$product->line->title}}</td>
+                                    <td>{{$product->brand->title}}</td>
                                     <td>{{$product->image}}</td>
-                                    <td><x-admin.extrabuttons :model="$product" resource="products"/></td>
+                                    <td><x-admin.extrabuttons :model="$product" resource="products"/><br><small>{{$product->created_at}}</small></td>
                                 </tr>
                             @endforeach
                         </tbody>
