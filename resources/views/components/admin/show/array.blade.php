a6cea4b54fa1f5b8ac5bba0c1e784ddf5a755c6f
dajamplay <ggyoug@gmail.com>
2023-11-07 10:41:58 +0300

    refactor: add css array show component

--- resources/views/components/admin/show/array.blade.php
+++ resources/views/components/admin/show/array.blade.php
@@ -8,11 +8,13 @@
     <div {!! $attributes->merge([])->class(['row single-item__row']) !!}>
         @if(is_iterable($array))
             <div class="col-md-2 single-item__title">Теги</div>
-            <div class="col-md-10 d-flex">
+
+            <div class="col-md-10 d-flex row">
                 @foreach($array as $elem)
-                    <div class="border border-primary rounded-pill p-1 m-1">{{ $elem->$key }}</div>
+                    <div class="border border-2 shadow-sm rounded-pill py-1 px-2 m-1 text-center">{{ $elem->$key }}</div>
                 @endforeach
             </div>
+
         @endisset
     </div>
 @endif
