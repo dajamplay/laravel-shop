a360d6c86b8b6b6fd2ed9224d017e1144427efa1
Maksim Aksenov <ggyoug@gmail.com>
2023-11-13 14:44:57 +0300

    feat: Feedback model

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -9,4 +9,16 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Feedback extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+    ];
+
+    protected $casts = [
+        'created_at' => "datetime:Y-m-d H:i:s",
+        'updated_at' => "datetime:Y-m-d H:i:s",
+        'deleted_at' => "datetime:Y-m-d H:i:s",
+    ];
 }
