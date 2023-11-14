742c38ef711bfd6a9fc070fd6999d1c27d37dd48
dajamplay <ggyoug@gmail.com>
2023-11-15 01:37:25 +0300

    feat: resize image

--- resources/views/components/admin/table/body/row/image.blade.php
+++ resources/views/components/admin/table/body/row/image.blade.php
@@ -1,12 +1,11 @@
 @props([
     'value' => null,
     'title' => null,
-    'height' => 100
+    'height' => 60,
 ])
 
 <td {{ $attributes->merge([])->class('')}}>
-    <a href="{{storage($value)}}" data-lightbox="{{$title}}"
-       data-title="{{$title}}">
+    <a href="{{storage($value)}}" data-lightbox="{{$title}}" data-title="{{$title}}">
         <img src="{{storage($value)}}" alt="{{$title}}" height="{{$height}}">
     </a>
 </td>
