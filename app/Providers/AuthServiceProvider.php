bfb7083c65b1b6e102b232b805ad54543d4d49f0
dajamplay <ggyoug@gmail.com>
2023-11-15 17:51:27 +0300

    refactor: delete policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,13 +4,13 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use App\Models\User;
-use App\Policies\UserPolicy;
+//use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
-        User::class => UserPolicy::class
+//        User::class => UserPolicy::class
     ];
 
     public function boot(): void
