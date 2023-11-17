2f16751392dc68c747ffc28ef122122d8ab87c00
dajamplay <ggyoug@gmail.com>
2023-11-17 17:49:51 +0300

    feat: add additional row for table with search and pagination

--- resources/views/components/admin/table/additional/index.blade.php
+++ resources/views/components/admin/table/additional/index.blade.php
@@ -8,25 +8,25 @@
 
 <div class="additional-table-block">
     <div class="additional-table-block__left">
-        <x-admin.button
-            text="{{$button_text}}"
-            href="{{$button_href}}"
-        />
-
-        <button type="submit" class="btn btn-md btn-primary">Поиск  <i class="fas fa-search"></i></button>
-
         <div>
-            <label for="{{$filter_trashed}}" class="btn btn-primary">Удаленные<i class="fas fa-trash"></i></label>
-            <input
-                type="checkbox"
-                id="{{$filter_trashed}}"
-                name="{{$filter_trashed}}"
-                {{ Request::query($filter_trashed) === 'on' ? 'checked' : ''}}
-                class="additional-table-block__left__checkbox"
-            />
+            <x-admin.button text="{{$button_text}}" href="{{$button_href}}"/>
+            <button type="submit" class="btn btn-md btn-primary">Применить фильтры <i class="fas fa-search"></i></button>
         </div>
 
-        <span class="btn btn-outline-primary mr-1">Найдено: {{ $total ?? 0 }}</span>
+        <div>
+            <div class="d-inline">
+                <label for="{{$filter_trashed}}" class="btn btn-sm btn-outline-danger">В корзине</label>
+                <input
+                    type="checkbox"
+                    id="{{$filter_trashed}}"
+                    name="{{$filter_trashed}}"
+                    {{ Request::query($filter_trashed) === 'on' ? 'checked' : ''}}
+                    class="additional-table-block__left__checkbox"
+                />
+            </div>
+            <span class="btn btn-outline-primary mr-1">Найдено: {{ $total ?? 0 }}</span>
+        </div>
+
     </div>
 
     <div class="additional-table-block__right">
@@ -53,8 +53,30 @@
         }
 
         .additional-table-block__left__checkbox {
-            transform : scale(2);
-            margin-left: 15px;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            appearance: none;
+            width: 20px;
+            height: 20px;
+            border: 1px solid red;
+            border-radius: 2px;
+            outline: none;
+            transition: background-color 0.3s ease-in-out;
+            line-height: 20px;
+            font-size: 20px;
+        }
+        .additional-table-block__left__checkbox:checked::before {
+            width: 20px;
+            height: 20px;
+            content: '\2714'; /* check mark symbol Unicode */
+            display: block;
+            text-align: center;
+            font-size: 20px;
+            line-height: 20px;
+            color: white;
+        }
+        .additional-table-block__left__checkbox:checked {
+            background-color: red;
         }
     </style>
 @endpushonce
