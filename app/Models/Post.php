9693337b564a132c36a3dbe30a278964c5c2a6e4
dajamplay <ggyoug@gmail.com>
2023-11-18 09:08:21 +0300

    feat: add post filterable interface

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Filterable;
 use App\Models\Traits\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,7 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory, SoftDeletes, HasSlug;
+    use HasFactory, SoftDeletes, HasSlug, Filterable;
 
     protected $fillable = [
         'title',
