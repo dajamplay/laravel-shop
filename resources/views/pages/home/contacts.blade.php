b56725376134c040a48c865fa399d6db91d5be4d
dajamplay <ggyoug@gmail.com>
2023-11-26 20:37:36 +0300

    feat: add delivery, contacts and about page

--- resources/views/pages/home/contacts.blade.php
+++ resources/views/pages/home/contacts.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
 
-    <h1>Контакты</h1>
+    <div class="container">
+        <div class="row">
+            <h1>Контакты</h1>
+            <hr>
+            <p>197110, Санкт-Петербург, ул. Большая Разночинная, дом 14, офис 516</p>
+            <p>Бизнес-центр "Бизнес Депо"</p>
+            <p>Ст.М. "Чкаловская"</p>
+            <p>Тел: +7(911)744-78-88, +7(911)940-66-95</p>
+            <p>E-mail: eleanta@yandex.ru</p>
+            <hr>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Afe87167dbac9cf72de4125c5fd85572fe53a708fe95801a4364f4e7811b2f8c4&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=false"></script>
+    </div>
 
 @endsection
