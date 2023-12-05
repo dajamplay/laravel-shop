7b4cb96ed77e3731653afc5208c83da23135b095
dajamplay <ggyoug@gmail.com>
2023-12-05 14:23:36 +0300

    refactor: delete product comments

--- resources/views/livewire/catalog-card-product.blade.php
+++ resources/views/livewire/catalog-card-product.blade.php
@@ -3,27 +3,7 @@
         <div class="product-img img-hover-effect">
             <a href="{{route('shop.products.show', $product->slug)}}">
                 <img class="primary-img" src="{{storage($product->image)}}" alt="Product Images">
-                {{--                                        <img class="secondary-img" src="{{storage($product->image)}}" alt="Product Images">--}}
             </a>
-{{--                                                <div class="product-add-action">--}}
-{{--                                                    <ul>--}}
-{{--                                                        <li>--}}
-{{--                                                            <a href="wishlist.html" data-tippy="Add to wishlist" data-tippy-inertia="true" data-tippy-animation="shift-away" data-tippy-delay="50" data-tippy-arrow="true" data-tippy-theme="sharpborder">--}}
-{{--                                                                <i class="pe-7s-like"></i>--}}
-{{--                                                            </a>--}}
-{{--                                                        </li>--}}
-{{--                                                        <li class="quuickview-btn" data-bs-toggle="modal" data-bs-target="#quickModal">--}}
-{{--                                                            <a href="#" data-tippy="Quickview" data-tippy-inertia="true" data-tippy-animation="shift-away" data-tippy-delay="50" data-tippy-arrow="true" data-tippy-theme="sharpborder">--}}
-{{--                                                                <i class="pe-7s-look"></i>--}}
-{{--                                                            </a>--}}
-{{--                                                        </li>--}}
-{{--                                                        <li>--}}
-{{--                                                            <a href="cart.html" data-tippy="Add to cart" data-tippy-inertia="true" data-tippy-animation="shift-away" data-tippy-delay="50" data-tippy-arrow="true" data-tippy-theme="sharpborder">--}}
-{{--                                                                <i class="pe-7s-cart"></i>--}}
-{{--                                                            </a>--}}
-{{--                                                        </li>--}}
-{{--                                                    </ul>--}}
-{{--                                                </div>--}}
         </div>
         <div class="product-content">
             <a class="product-name" href="{{$product->slug}}">{{$product->title}}</a>
@@ -44,13 +24,6 @@
                     <li><i class="fa fa-star"></i></li>
                     <li><i class="fa fa-star"></i></li>
                 </ul>
-            </div>
-            <div>
-{{--                @if(count($tagsList) > 0)--}}
-{{--                    @foreach($tagsList as $tag)--}}
-{{--                        <p>{{$tag}}</p>--}}
-{{--                    @endforeach--}}
-{{--                @endif--}}
             </div>
         </div>
     </div>
