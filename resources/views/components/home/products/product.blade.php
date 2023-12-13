6486f9b202a545b1eec55a38c54846c676a423b1
dajamplay <ggyoug@gmail.com>
2023-12-13 15:10:28 +0300

    fix: links to single product

--- resources/views/components/home/products/product.blade.php
+++ resources/views/components/home/products/product.blade.php
@@ -9,7 +9,7 @@
         </a>
 
         <div class="card-body p-2 text-center">
-            <h5 class="card-title"><a href="{{$product->slug}}">{{$product->title}}</a></h5>
+            <h5 class="card-title"><a href="{{route('shop.products.show', $product->slug)}}">{{$product->title}}</a></h5>
             <div class="price-box pb-1">
                 <span class="new-price">{{$product->price}} Руб.</span>
             </div>
