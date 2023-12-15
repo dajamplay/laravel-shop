1c2a3a11610f30e47f39dfb75f9670a4dd7eb93c
dajamplay <ggyoug@gmail.com>
2023-12-15 23:43:50 +0300

    feat: add admin color table yes or not

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -18,7 +18,7 @@
             <x-admin.table.head.text title="Цена" filter="filter_price"/>
             <x-admin.table.head.text title="Цена(опт)" filter="filter_price_opt"/>
             <x-admin.table.head.text title="Объем" filter="filter_size"/>
-            <x-admin.table.head.text title="Описание" filter="filter_content"/>
+            <x-admin.table.head.text title="Текст"/>
             <x-admin.table.head.text title="Линия" filter="filter_line"/>
             <x-admin.table.head.text title="Бренд" filter="filter_brand"/>
             <x-admin.table.head.text title="Изображение"/>
@@ -37,7 +37,7 @@
                         <x-admin.table.body.row.money value="{{$product->price}}"/>
                         <x-admin.table.body.row.money value="{{$product->price_opt}}"/>
                         <x-admin.table.body.row.size value="{{$product->size}}"/>
-                        <x-admin.table.body.row.text value="{!!$product->content!!}" limit="40"/>
+                        <x-admin.table.body.row.text value="{!!$product->content ? 'да' : 'нет'!!}" limit="40"/>
                         <x-admin.table.body.row.text value="{{$product->line->title}}"/>
                         <x-admin.table.body.row.text value="{{$product->brand->title}}"/>
                         <x-admin.table.body.row.image value="{{$product->image}}" title="{{$product->title}}"/>
--- resources/views/components/admin/table/body/row/text.blade.php
+++ resources/views/components/admin/table/body/row/text.blade.php
@@ -5,7 +5,13 @@
 
 <td {{ $attributes->merge([])->class([])}}>
     @if(!empty($value))
-        {!! $limit ? Str::limit($value, $limit, ' ...') : $value !!}
+        @if($value === 'нет')
+            <span class="text-danger">нет</span>
+        @elseif($value === 'да')
+            <span class="text-success">да</span>
+        @else
+            {!! $limit ? Str::limit($value, $limit, ' ...') : $value !!}
+        @endif
     @else
         <x-admin.table.body.row.empty />
     @endif
