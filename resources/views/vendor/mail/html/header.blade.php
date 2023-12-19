2d7980710fbf98439fdb6f67a90ff22431ef5d50
dajamplay <ggyoug@gmail.com>
2023-12-19 09:49:05 +0300

    feat: add logo to email layout

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,16 +2,9 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
     <div>
-        {{ $slot }}
+        <img src="{{ asset('assets/images/logo.png') }}" class="logo" alt="Елеанта">
     </div>
-    <div>
-        <img src="{{ asset('adminlte/img/avatars/avatar.png') }}" class="logo" alt="Laravel Logo">
-    </div>
-@endif
 </a>
 </td>
 </tr>
