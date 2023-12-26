3f5bc64af17d1441bf51830725693b203ea3b4e9
dajamplay <ggyoug@gmail.com>
2023-12-26 18:34:53 +0300

    fix: rename alert message for livewire

--- app/Livewire/Alert/Alert.php
+++ app/Livewire/Alert/Alert.php
@@ -12,7 +12,7 @@ class Alert extends Component
 
     public bool $isOpen = false;
 
-    #[On('modal')]
+    #[On('message')]
     public function openModal(string $message): void
     {
         $this->message = $message;
